eebcfd38afcc63737863711d0894084803259032
Gagan P S <4bd23is040@bietdvg.edu>
2026-07-02 16:22:33 +0530

    Add files via upload

---
+++ week 2/module 5/Exercise 4/LibraryManagement/src/main/java/com/library/App.java
@@ -0,0 +1,13 @@
+package com.library;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercise 4 - Maven Project Configured Successfully");
+        System.out.println("Spring Context Added");
+        System.out.println("Spring AOP Added");
+        System.out.println("Spring Web MVC Added");
+
+    }
+}
